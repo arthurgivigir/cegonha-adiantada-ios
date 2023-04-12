c285ecf2037c9fe9bdf6af9fe8a8ac1221589332
Arthur Givigir <arthur316@gmail.com>
2023-04-12 19:11:41 -0300

    🎉 Begin project! Go go go

---
+++ CegonhaAdiantada/CegonhaAdiantada/CegonhaAdiantadaApp.swift
@@ -0,0 +1,20 @@
+//
+//  CegonhaAdiantadaApp.swift
+//  CegonhaAdiantada
+//
+//  Created by Arthur Givigir on 12/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CegonhaAdiantadaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
