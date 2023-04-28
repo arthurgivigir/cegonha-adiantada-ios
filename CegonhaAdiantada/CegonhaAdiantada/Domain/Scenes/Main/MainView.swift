171f08e9a3af2047c924e2ee5a06933302d96fd9
Arthur Givigir <arthur316@gmail.com>
2023-04-28 10:04:34 -0300

    🏗  Fixed tab layout

--- CegonhaAdiantada/CegonhaAdiantada/Domain/Scenes/Main/MainView.swift
+++ CegonhaAdiantada/CegonhaAdiantada/Domain/Scenes/Main/MainView.swift
@@ -9,29 +9,28 @@ import SwiftUI
 
 struct MainView: View {
     @ObservedObject var birthCalculation = BirthCalculationDataStore()
-    @State var selectedTab: Bool = true
+    @State var selectedTab: Tab = .home
     
     var body: some View {
         NavigationView {
             VStack {
-                if selectedTab {
+                if selectedTab == .home {
                     BirthCalculationView(birthCalculation: birthCalculation)
                         .configureView()
-//                        .environmentObject(birthCalculation)
-    //                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
                 } else {
-                    Text("Teste")
-    //                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                    Rectangle()
+                        .fill(.black)
                 }
             }
             .overlay(alignment: .bottom) {
                 TabBarView(selectedTab: $birthCalculation.selectedTabBar)
                     .padding(.bottom, 20)
                     .onChange(of: birthCalculation.selectedTabBar) { newValue in
-                        withAnimation(.easeInOut(duration: 0.2)) {
-                            selectedTab.toggle()
+                        withAnimation(.easeInOut) {
+                            selectedTab = newValue
                         }
                     }
+                    .frame(alignment: .bottom)
             }
             .ignoresSafeArea(.keyboard)
         }
